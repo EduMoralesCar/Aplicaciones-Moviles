8fe6099c7e8e2dbe5ea43ecb86964c0c474a5a4b
Edomocar <edumoralescarlos@gmail.com>
2026-09-03 20:05:48 -0500

    fix: configurar dependencias de AsyncStorage y punto de entrada para App01

--- proyecto06-app/index.js
+++ proyecto06-app/index.js
@@ -1,8 +1,11 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+// import App from './App';
+import App01 from './App01';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+
+// registerRootComponent(App);
+registerRootComponent(App01);
